d92fd211e76d5fff453cf8b7080739cfa144168b
Pelle Hald Vedsmand <cph-pv73@cphbusiness.dk>
2025-03-03 14:07:56 +0100

    Added beforeInsertRegistration trigger

---
+++ task3_filer/beforeInsertRegistration.sql
@@ -0,0 +1,19 @@
+DELIMITER //
+create trigger beforeInsertRegistration
+before insert on tournament_registrations
+for each row
+begin
+	declare playerCount int;
+    declare maxPlayers int;
+    
+    select count(*) into playerCount from tournament_registrations where tournament_id = NEW.tournament_id;
+    select max_players into maxPlayers from tournaments where tournament_id = NEW.tournament_id;
+    
+    IF playerCount>= maxPlayers then
+		signal sqlstate '45000' set message_text = 'Tournament Full';
+	
+    end if;
+    
+end //
+
+DELIMITER ;
